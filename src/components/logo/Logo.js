60239d32b3df96d4ced1040e79b0cbe12cca5bbb
Syed Shakil Hasan <skshakil591@gmail.com>
2024-03-23 23:13:58 +0600

    Completed Full Site

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,30 +5,51 @@ const useStyles = makeStyles((theme) => ({
   svgHover: {
     fill: theme.palette.foreground.default,
     "&:hover": {
-        fill: theme.palette.primary.main,
+      fill: theme.palette.primary.main,
     },
     transition: "all 0.5s ease",
   },
 }));
 
 export const Logo = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-      <svg 
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="-35 -10 220 220"
-        className={classes.svgHover}
-      >
-        <g id="Layer_2" data-name="Layer 2">
-          <g id="Layer_5" data-name="Layer 5">
-            <rect className="cls-1" x="0.47" y="31.33" width="9.9" height="133.85" rx="3.75"/>
-            <rect className="cls-1" x="133.62" y="57.1" width="9.9" height="108.08" rx="3.75"/>
-            <rect className="cls-1" x="30.39" y="21.71" width="9.9" height="112.54" rx="3.75" transform="translate(-38.83 35.21) rotate(-35.65)"/>
-            <rect className="cls-1" x="90.6" y="-17.08" width="9.9" height="201.44" rx="3.75" transform="translate(65.78 -39.81) rotate(35.24)"/>
-            <rect className="cls-1" x="78.17" y="39.68" width="9.9" height="201.55" rx="3.75" transform="translate(96.28 -22.22) rotate(35.24)"/>
-          </g>
-        </g>
-      </svg>
-    );
+  return (
+    <svg
+      className={classes.svgHover}
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.0"
+      viewBox="-9 -12 100 100"
+    >
+      <defs>
+        <clipPath id="6a20aae007">
+          <path
+            fill="#000000"
+            className="cls-1"
+            d="M 24 9.238281 L 61.765625 9.238281 L 61.765625 56 L 24 56 Z M 24 9.238281 "
+            clip-rule="nonzero"
+          />
+        </clipPath>
+        <clipPath id="1ccf8daac6">
+          <path
+            className="cls-1"
+            d="M 13.015625 19 L 51 19 L 51 65.488281 L 13.015625 65.488281 Z M 13.015625 19 "
+            clip-rule="nonzero"
+          />
+        </clipPath>
+      </defs>
+      <g clip-path="url(#6a20aae007)" id="Layer_2" data-name="Layer 2">
+        <path
+          className="cls-1"
+          d="M 31.800781 20.117188 L 37.496094 16.828125 L 61.96875 30.957031 L 61.96875 23.367188 L 37.496094 9.238281 L 24.574219 16.699219 L 24.574219 24.613281 L 55.394531 42.40625 L 55.394531 55.421875 L 61.96875 51.625 L 61.96875 37.539062 Z M 31.800781 20.117188 "
+        />
+      </g>
+      <g clip-path="url(#1ccf8daac6)" id="Layer_5" data-name="Layer 5">
+        <path
+          className="cls-1"
+          d="M 43.195312 54.875 L 37.496094 58.164062 L 13.027344 44.035156 L 13.027344 51.625 L 37.496094 65.757812 L 50.421875 58.292969 L 50.421875 50.378906 L 19.601562 32.589844 L 19.601562 19.570312 L 13.027344 23.367188 L 13.027344 37.457031 Z M 43.195312 54.875 "
+        />
+      </g>
+    </svg>
+  );
 };
--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -5,19 +5,20 @@ import { Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 
-import './Works.css';
+import "./Works.css";
 
 // Import ../../assets/recentprojects/
-import Portfolio from '../../assets/recentprojects/react-portfolio.png';
-import Veritru from '../../assets/recentprojects/veritru.png';
-import Lofo from '../../assets/recentprojects/lofo.png';
-import Startup from '../../assets/recentprojects/startup.png';
-import Lacalle from '../../assets/recentprojects/lacalle.png';
+import applewebsite from "../../assets/recentprojects/apple-website.png";
+import schoolsite from "../../assets/recentprojects/school-site.png";
+import shoeecommerce from "../../assets/recentprojects/shoe-ecommerce.png";
+import music from "../../assets/recentprojects/music.png";
+import beauty from "../../assets/recentprojects/beauty.png";
+import softstok from "../../assets/recentprojects/softstok.png";
 
 const useStyles = makeStyles((theme) => ({
   main: {
-    maxWidth: '100vw',
-    marginTop: '3em',
+    maxWidth: "100vw",
+    marginTop: "3em",
     marginBottom: "auto",
   },
 }));
@@ -25,48 +26,48 @@ const useStyles = makeStyles((theme) => ({
 export const Works = () => {
   const classes = useStyles();
   const [projects, setProjects] = useState([
-    { 
+    {
       id: 1,
-      title: 'React Portfolio', 
-      description: `Designed and developed a ReactJS portfolio 
-      with fancy 3D animations using Three.js for 
-      the background element.`,
-      alter: 'React Portfolio',
-      image: `${Portfolio}`,
+      title: "Software Company",
+      description: `Unlock boundless possibilities with SoftStok. Empowering businesses with innovative software solutions for seamless operations and sustainable growth.`,
+      alter: "React Portfolio",
+      image: `${softstok}`,
     },
-    { 
+    {
       id: 2,
-      title: 'VeriTru Project', 
-      description: `An advocacy project website built using
-      MEAN stack with fact-checking tool to promote actions against
-      fake news.`,
-      alter: 'VeriTru Project',
-      image: `${Veritru}`,
+      title: "Apple Brand Store",
+      description: `I have created a super modern and professional website for an Apple brand store. Elevate your shopping experience with us.`,
+      alter: "React Portfolio",
+      image: `${applewebsite}`,
     },
-    { 
+    {
       id: 3,
-      title: 'LoFo Project', 
-      description: `Logistics and Forwarding website built using
-      ReactJS to design and develop its front-end.`,
-      alter: 'LoFo Project',
-      image: `${Lofo}`,
+      title: "Fauget School",
+      description: `Empowering education for all. Navigate curriculum, events, and communication effortlessly on our innovative school website.`,
+      alter: "Fauget School",
+      image: `${schoolsite}`,
     },
-    { 
+    {
       id: 4,
-      title: 'Startup Project', 
-      description: `A website portfolio project for the Startup Dev Team
-      built using MEVN stack to demonstrate the CRUD capabilities of the tech stack.`,
-      alter: 'Startup Project',
-      image: `${Startup}`,
+      title: "Mordan Ecommerce ",
+      description: `Step into style with our modern e-commerce shoe store. Explore a curated collection of trendy footwear for every occasion, all just a click away.`,
+      alter: "Mordan Ecommerce",
+      image: `${shoeecommerce}`,
     },
-    { 
+    {
       id: 5,
-      title: 'LaCalle Cafe', 
-      description: `A website project for the La Calle Cafe business
-      built using Wordpress and PHP with integrated SEO tools to help
-      the business ramp up its prospects and lead generation.`,
-      alter: 'Startup Project',
-      image: `${Lacalle}`,
+      title: "The Arrowai",
+      description: `Immerse yourself in the world of music with our platform. Discover new artists, explore diverse genres, and enjoy seamless listening experiences tailored to your taste.`,
+      alter: "Music Platforms",
+      image: `${music}`,
+    },
+    {
+      id: 6,
+      title: "Beauty Care ",
+      description: `Indulge in the ultimate beauty experience with our platform. Discover curated skincare, makeup, and wellness products, tailored to enhance your natural radiance and confidence.`,
+      alter:
+        "Indulge in the ultimate beauty experience with our platform. Discover curated skincare, makeup, and wellness products, tailored to enhance your natural radiance and confidence.",
+      image: `${beauty}`,
     },
   ]);
 
@@ -74,17 +75,15 @@ export const Works = () => {
     <section id="works">
       <Container component="main" className={classes.main} maxWidth="md">
         {projects.map((project) => (
-          <div className="project" key={ project.id }>
+          <div className="project" key={project.id}>
             <div className="__img_wrapper">
-              <img src={ project.image } alt={ project.alter }/>
+              <img src={project.image} alt={project.alter} />
             </div>
             <div className="__content_wrapper">
               <h3 className="title">
-                <TextDecrypt text={ project.id + '. ' + project.title } />
+                <TextDecrypt text={project.id + ". " + project.title} />
               </h3>
-              <p className="description">
-                { project.description }
-              </p>
+              <p className="description">{project.description}</p>
             </div>
           </div>
         ))}
